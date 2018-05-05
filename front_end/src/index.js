3947def42f1c8d76b54ed7626bc7117f438f7b6f
sranjith <ranjith1210000@gmail.com>
2018-05-05 10:55:29 +0200

    imports

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 ReactDOM.render(
     <App />
