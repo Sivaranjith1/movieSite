42b1bae4e1ce7b925363eaafcb502cb90b5ee20e
sranjith <ranjith1210000@gmail.com>
2018-05-24 18:16:13 +0200

    just decentural url pattern

--- front_end/src/actions/fetch_movies.js
+++ front_end/src/actions/fetch_movies.js
@@ -1,5 +1,7 @@
+import { url } from '../config';
+
 export const fetch_movies = () => dispatch => {
-    fetch('http://127.0.0.1.xip.io:8000/movie/all')
+    fetch(`${url}/movie/all`)
     .then(res => res.json())
     .then(res => dispatch({type: "FETCH_MOVIE", payload: res}))
 }
