c994c6b3c5c8cbb7cc9c061811fc9edb2cf2c5c4
sranjith <ranjith1210000@gmail.com>
2018-05-24 21:18:56 +0200

    added fetch_serie

--- front_end/src/reducers/movieList.js
+++ front_end/src/reducers/movieList.js
@@ -11,6 +11,11 @@ export default function (state=initialState, action) {
                 ...state.movies, ...action.payload
             ]};
 
+        case 'FETCH_SERIE':
+            return {...state, serie: [
+                ...state.serie, ...action.payload
+            ]}
+
         default:
             return state;
     }
