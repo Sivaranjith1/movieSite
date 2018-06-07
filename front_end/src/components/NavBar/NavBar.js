9c677abbb95cb72f6b669b999b74405cef0d7a79
sranjith <ranjith1210000@gmail.com>
2018-06-07 19:37:51 +0200

    error with the withStyles func

--- front_end/src/components/NavBar/NavBar.js
+++ front_end/src/components/NavBar/NavBar.js
@@ -1,53 +1,16 @@
-/*
-import React from 'react'
-import AppBar from 'material-ui/AppBar'
-import Toolbar from 'material-ui/Toolbar'
-import Divider from 'material-ui/Divider'
-//import Typography from 'material-ui/Typography'
-import IconButton from 'material-ui/IconButton'
-import MenuIcon from 'material-ui/Menu'
-
-const NavBar = () => {
-  return (
-      <div>
-        <AppBar position="static" color="default">
-          <Toolbar>
-            
-          </Toolbar>
-        </AppBar>
-      </div>
-  )
-}
-
-<Toolbar >
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={this.handleDrawerOpen}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" noWrap>
-              Netflix
-            </Typography>
-          </Toolbar>
-
-export default NavBar;
-*/
 import React from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/styles/typography';
 
-const styles = {
+const styles = theme => ({
   root: {
     flexGrow: 1,
   },
-};
+});
 
-function NavBar(props) {
+const NavBar = (props) => {
   const { classes } = props;
   return (
     <div className={classes.root}>
@@ -63,3 +26,4 @@ function NavBar(props) {
 }
 
 export default withStyles(styles)(NavBar);
+//export default NavBar;
