1e27060c6c87fa557f3fde53dcf61f869dbd9c09
sranjith <ranjith1210000@gmail.com>
2018-06-07 11:30:46 +0200

    error

--- front_end/src/components/NavBar/NavBar.js
+++ front_end/src/components/NavBar/NavBar.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Divider from 'material-ui/Divider'
+import Typography from 'material-ui/styles/typography'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui/Menu'
 
 const NavBar = () => {
   return (
-      <AppBar title="Video Streaming" />
+      <AppBar>
+        <Toolbar >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={this.handleDrawerOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" noWrap>
+            Netflix
+          </Typography>
+        </Toolbar>
+      </AppBar>
   )
 }
 
