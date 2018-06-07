d82dec5bf586653f61add472a4ec327abfa26732
sranjith <ranjith1210000@gmail.com>
2018-06-07 15:41:32 +0200

    more error

--- front_end/src/components/NavBar/NavBar.js
+++ front_end/src/components/NavBar/NavBar.js
@@ -1,28 +1,65 @@
+/*
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Divider from 'material-ui/Divider'
-import Typography from 'material-ui/styles/typography'
+//import Typography from 'material-ui/Typography'
 import IconButton from 'material-ui/IconButton'
 import MenuIcon from 'material-ui/Menu'
 
 const NavBar = () => {
   return (
-      <AppBar>
-        <Toolbar >
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={this.handleDrawerOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit" noWrap>
-            Netflix
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            
+          </Toolbar>
+        </AppBar>
+      </div>
   )
 }
 
+<Toolbar >
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Netflix
+            </Typography>
+          </Toolbar>
+
 export default NavBar;
+*/
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/styles/typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
+
+function NavBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            Title
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default withStyles(styles)(NavBar);
