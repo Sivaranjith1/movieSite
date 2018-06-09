def6db0c270281e4317337263f4373c413c513ae
sranjith <ranjith1210000@gmail.com>
2018-06-09 19:26:38 +0200

    Navbar look finised

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
+import { grey } from '@material-ui/core/colors';
 
 import NavBar from './components/NavBar/NavBar';
 import MovieList from './components/grid/MovieList'
@@ -9,7 +9,7 @@ import MovieList from './components/grid/MovieList'
 
 const theme = createMuiTheme({
   palette: {
-    primary: red
+    primary: grey,
   }
 })
 
--- front_end/src/components/NavBar/NavBar.js
+++ front_end/src/components/NavBar/NavBar.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close'
 import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List'
+import { ListItem, ListItemText, ListItemIcon, Divider } from '@material-ui/core/'
 
-const styles = ({palette, spacing}) => ({
+
+const styles = theme => ({
   root: {
     flexGrow: 1,
+  },
+  drawer: {
+    width: 360
   },
   menuButton: {
 
   },
   colorTool: {
-    backgroudColor: palette.primary.main,
-  }
+    backgroundColor: theme.palette.primary.main,
+  },
 });
 
 class NavBar extends React.Component{
@@ -43,8 +50,46 @@ class NavBar extends React.Component{
             </Typography>
           </Toolbar>
         </AppBar>
+
         <Drawer open={this.state.isOpen} onClose={() => this.setState({isOpen: !this.state.isOpen})}>
-          hei
+          <div className={classes.drawer}>
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <CloseIcon onClick={() => this.setState({isOpen: !this.state.isOpen})} />
+                </ListItemIcon>
+              </ListItem>
+              <Divider />
+              <ListItem button component="a">
+                <ListItemText>
+                  Home
+                </ListItemText>
+              </ListItem>
+              <ListItem button component="a">
+                <ListItemText>
+                  Movies
+                </ListItemText>
+              </ListItem>
+              <ListItem button component="a">
+                <ListItemText>
+                  Series
+                </ListItemText>
+              </ListItem>
+
+              <Divider />
+
+              <ListItem button component="a">
+                <ListItemText>
+                  Watched
+                </ListItemText>
+              </ListItem>
+              <ListItem button component="a">
+                <ListItemText>
+                  Hearted
+                </ListItemText>
+              </ListItem>
+            </List>
+          </div>
         </Drawer>
       </div>
     );
@@ -52,4 +97,3 @@ class NavBar extends React.Component{
 }
 
 export default withStyles(styles)(NavBar);
-//export default NavBar;
